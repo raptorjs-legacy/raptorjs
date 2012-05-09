5437b5e4120d64203b2e738cdb55367b5f783a33
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-05-09 17:20:20 -0600

    Updating demo

--- src/test/javascript/demo-app/demo.js
+++ src/test/javascript/demo-app/demo.js
@@ -7,21 +7,39 @@ require('raptorjs').createRaptor({
                     path: __dirname + '/resources'
                 }
             ]
+        },
+        logging: {
+            loggers: {
+                'ROOT': {level: 'WARN'},
+                'oop-server': {level: 'WARN'},
+                'resources': {level: 'WARN'}
+            }
         }
 });
 
-var templateResource = raptor.require("resources").findResourceSync("/demo.rhtml");
-var templateSrc = templateResource.readFullySync();
-var compiler = raptor.require("templating.compiler").createCompiler({templateName: "demo"});
-var compiledSrc = compiler.compile(templateSrc, "/demo.rhtml");
-//console.log(compiledSrc);
-
-var templating = raptor.require("templating");
-eval(compiledSrc);
-
-var html = templating.renderToString("demo", {});
-console.log(html);
-
+var logger = raptor.require('logging').logger("demo");
+try
+{
+    var templateResource = raptor.require("resources").findResourceSync("/demo.rhtml");
+    var templateSrc = templateResource.readFullySync();
+    var compiler = raptor.require("templating.compiler").createCompiler({templateName: "demo"});
+    var compiledSrc = compiler.compile(templateSrc, "/demo.rhtml");
+    console.log(compiledSrc);
+    
+    var templating = raptor.require("templating");
+    eval(compiledSrc);
+    
+    templating.helpers.myHelperFunction = function(data) {
+        return "Output of my helper function (" + data.name + ")";
+    };
+    
+    var html = templating.renderToString("demo", {name: "TEST", colors: ['red', 'green', 'blue', 'purple']});
+    console.log(html);
+}
+catch(e) {
+    //console.error(e);
+    logger.error(e);
+}
 //console.log(raptor.require("templating").renderToString("test", {
 //  message: "Hello World", 
 //  colors: ["red", "green", "blue", "pink"], 
