f204f36cd6358802df7ff5ffc3c19ddf5858db7f
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-05-17 10:19:34 -0600

    Updated add-license tool and added license to all Java source files

--- tools/add-license/add-license-app.js
+++ tools/add-license/add-license-app.js
@@ -13,31 +13,45 @@ var files = raptor.require("files"),
 
 var licenseText = files.readFully(__dirname + "/LICENSE");
 
-var srcDir = files.joinPaths(__dirname, "../../src/main/resources/META-INF/resources/raptorjs_modules");
-var walker = raptor.require("files.walker");
-console.log("Source directory: " + srcDir);
+var raptorJSModulesDir = files.joinPaths(__dirname, "../../src/main/resources/META-INF/resources/raptorjs_modules");
+var javaSrcDir = files.joinPaths(__dirname, "../../src/main/java");
 
-walker.walk(
-        srcDir, 
-        function(file) {
-            console.log("Processing " + file.getAbsolutePath() + "...");
-            var input = file.readFully();
-            if (!strings.startsWith(input, licenseText)) {
-                var output = licenseText + file.readFully();
-                file.writeFully(output);
-            }
-            else {
-                console.log("Skipping " + file.getAbsolutePath() + ". License already added");
-            }
-            
-        }, 
-        this,
-        {
-            fileFilter : function(file) {
-                return file.isFile() && 
-                    file.getExtension() == "js";
-            },
-            dirTraverseFilter : function(dir) {
-                return dir.getName() !== "node_modules";
-            }
-        });
+var walker = raptor.require("files.walker");
+
+var count = 0;
+
+var addLicense = function(srcDir, ext) {
+    console.log("Processing source directory " + srcDir + "...");
+    
+    walker.walk(
+            srcDir, 
+            function(file) {
+                console.log("Processing " + file.getAbsolutePath() + "...");
+                var input = file.readFully();
+                if (!strings.startsWith(input, licenseText)) {
+                    count++;
+                    var output = licenseText + file.readFully();
+                    file.writeFully(output);
+                }
+                else {
+                    console.log("Skipping " + file.getAbsolutePath() + ". License already added");
+                }
+                
+            }, 
+            this,
+            {
+                fileFilter : function(file) {
+                    return file.isFile() && 
+                        file.getExtension() == ext;
+                },
+                dirTraverseFilter : function(dir) {
+                    return dir.getName() !== "node_modules";
+                }
+            });
+    console.log();
+};
+
+addLicense(raptorJSModulesDir, "js");
+addLicense(javaSrcDir, "java");
+
+console.log("DONE. License added to " + count + " files.");
