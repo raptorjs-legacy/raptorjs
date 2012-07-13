bdc7353072b6db17756066263d7ca74cf3ffae68
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-07-13 15:38:52 -0600

    Adding jshint to "npm test" script

--- run-jshint.sh
+++ run-jshint.sh
@@ -1,6 +1,6 @@
 CUR_DIR=`dirname ${0}`
 RAPTORJS_ROOT=${CUR_DIR}
-JS_DIR=${RAPTORJS_ROOT}/src/main/resources/META-INF/resources/raptorjs_modules
+JS_DIR=${RAPTORJS_ROOT}/src/main/resources/META-INF/resources/raptor_modules
 
 pushd ${JS_DIR} > /dev/null
 ${RAPTORJS_ROOT}/node_modules/jshint/bin/hint .
