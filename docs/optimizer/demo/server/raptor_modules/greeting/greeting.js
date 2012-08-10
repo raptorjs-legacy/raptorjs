2c048a04c9783d3f440128e048d42c00897ec932
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-08-09 21:59:46 -0700

    Updating taglib namespaces in docs

--- docs/optimizer/demo/server/raptor_modules/greeting/greeting.js
+++ docs/optimizer/demo/server/raptor_modules/greeting/greeting.js
@@ -1,6 +1,9 @@
 raptor.define("greeting", function() {
     return {
         renderToEl: function(el) {
+            
+            el.invalidMethod();
+            
             var message = "Hello from 'greeting'!";
             var templating = raptor.require("templating");
             var html = templating.renderToString("greeting", {message: message});
