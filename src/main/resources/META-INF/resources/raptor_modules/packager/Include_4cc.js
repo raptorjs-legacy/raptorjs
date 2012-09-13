9f459dc8fd1b118337d608948d1f6518b56c8753
Rich Heisterberg <rheisterberg@ebay.com>
2012-09-13 07:01:43 -0700

    Temporary fix for 4cc files

---
+++ src/main/resources/META-INF/resources/raptor_modules/packager/Include_4cc.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2011 eBay Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+raptor.defineClass(
+    "packager.Include_4cc",
+    "packager.Include",
+    function(raptor) {
+        "use strict";
+        
+        return {
+            getKey: function() {
+                return "4cc:" + this.resolvePathKey(this.path);
+            },
+            
+            toString: function() {
+                return this.getResource().getPath();
+            },
+            
+            getCode: function(context) {
+                return this.getResource(context).readFully();
+            },
+           
+            getResourcePath: function() {
+                return this.path;
+            },
+            
+            getContentType: function() {
+                return "application/content";
+            },
+            
+            isInPlaceDeploymentAllowed: function() {
+                return true;
+            },
+            
+            load: function(context) {
+            }
+            
+        };
+    });
