b36b515e36b76f1f6966c6f905545067310cf5c6
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-10-02 13:01:46 -0600

    Updated CommentParser to better handle whitespace removal

--- test/raptor-jsdoc-spec.js
+++ test/raptor-jsdoc-spec.js
@@ -121,7 +121,7 @@ describe('strings module', function() {
     	expect(comment.hasTag('test')).toEqual(true);
     	expect(comment.getTag('test').getValue()).toEqual('test');
     	
-    	expect(comment.getTag('anotherTag').getValue()).toEqual('line1\nline2\nline3');
+    	expect(comment.getTag('anotherTag').getValue()).toEqual('line1 line2 line3');
     	
     	expect(comment.hasTag('novalue')).toEqual(true);
     	expect(comment.getTag('novalue').getValue()).toStrictlyEqual('');
@@ -130,7 +130,15 @@ describe('strings module', function() {
     	expect(comment.getTag('novalue2').getValue()).toStrictlyEqual('');
     	
     	expect(comment.getDescription()).toEqual('Hello');
-    })
+    });
+    
+    it('should allow complex comments to be parsed', function() {
+        var comment = parseComment('resources/jsdoc/comment-complex.js');
+        expect(comment.getTags("param").length).toEqual(3);
+        expect(comment.getTags("returns").length).toEqual(1);
+        expect(comment.getTags("param")[2].getValue()).toEqual("factory A factory function that returns either the class constructory function (with prototype) or just the prototype");
+        expect(comment.getDescription()).toEqual("Defines a class. This is identical to identical to \"define\" except that it supports a short-hand notation for classes\n\nMultiple signatures supported:\n<ul>\n<li>defineClass(name, modifiers, factory)\n<li>defineClass(name, superclassName, factory)\n<li>defineClass(name, factory)\n<li>defineClass(modifiers, factory)\n<li>defineClass(factory)\n</ul>\n\nSupported modifiers:\n<ul>\n<li>superclass: The name of the super class\n<li>mixins: An array of names of mixins\n</ul>\n\nIn addition, the \"modifiers\" parameter can be a string that specifies the name of the superclass\n\n<h2>Examples: Class with prototype</h2>\n<js>\nraptor.defineClass(\n    'some.namespace.MyClass',\n    function() {\n        return {\n            init: function() {\n                //Constructor \n            },\n            \n            //Prototype methods:\n            someMethod: function() { ... }\n        }\n    });\n</js>");
+    });
     
 
 });
