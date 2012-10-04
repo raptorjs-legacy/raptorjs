ee753841506e05a8541a61d5ca216c85af50f658
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-10-04 14:09:25 -0600

    Updated jsdocs for the optimizer module

--- src/main/resources/META-INF/resources/raptor_modules/optimizer/optimizer.js
+++ src/main/resources/META-INF/resources/raptor_modules/optimizer/optimizer.js
@@ -1,3 +1,25 @@
+/**
+ * The optimizer module is used to generated optimized
+ * web pages--including optimized resource bundles and the HTML markup
+ * to include the optimized resource bundles in an HTML page.
+ * 
+ * Simple usage:
+ * <js>
+ * var optimizer = raptor.require('optimizer');
+ * optimizer.configure('path/to/optimizer-config.xml', params);
+ * var optimizedPage = optimizer.optimizePage({
+ *         name: "test-page",
+ *         packageManifest: 'path/to/test-page-package.json'
+ *     });
+ * 
+ * var headHtml = optimizedPage.getSlotHtml('head');
+ * var bodyHtml = optimizedPage.getSlotHtml('body');
+ * var loaderMeta = optimizedPage.getLoaderMetadata();
+ * ...
+ * </js>
+ * 
+ * For more information, please see the <a href="http://raptorjs.org/optimizer/">Optimizer Documentation</a>.
+ */
 raptor.define(
     "optimizer", 
     function(raptor) {
@@ -16,22 +38,61 @@ raptor.define(
         defaultConfig.setUrlPrefix('/static/');
 
         var optimizer = {
+            /**
+             * @type optimizer.Config
+             */
             defaultConfig: defaultConfig,
+            
+            /**
+             * @type optimizer.PageOptimizer
+             */
             pageOptimizer: null,
 
+            /**
+             * Optimizes a page based on the page options provided. 
+             * 
+             * Supported options:
+             * <ul>
+             *     <li><b>name</b> {String}: The name of the page. This property is used to determine the name for page bundles and it is also used for any page-specific configuration options. [REQUIRED]
+             *     <li><b>basePath</b> {String}: A directory name used to generate relative URLs to resource bundles. The base path will typically be the output directory for the page. This option is ignored if the optimizer is configured to use a URL prefix.
+             *     <li><b>enabledExtensions</b> {Array|Object|{@link packager.PackageExtension}}: A collection of extensions that should be enabled when generating the optimized page bundles
+             *     <li><b>packageManifest</b> {{@link packager.PackageManifest}|{@link resources.Resource}|{@link files.File}|String}: A package manifest for the page that describes the page dependencies
+             * </ul>
+             * 
+             * @param  {options} options Information about the page being optimized (see above for supported options)
+             * @return {optimizer.OptimizedPage} The object that represents the optimized page.
+             */
             optimizePage: function(options) {
                 return this.pageOptimizer.optimizePage(options);
             },
 
+            /**
+             * Resets the optimizer back to the default configuration.
+             */
             configureDefault: function() {
                 this.pageOptimizer = new PageOptimizer(defaultConfig);
             },
             
+            /**
+             * Configures the default page optimizer instance using the provided
+             * configuration and configuration params.
+             * 
+             * @param  {optimizer.Config|String|files.File|resources.Resource} config The configuration to use
+             * @param  {Object} params An object with name/value pairs that are used for variable substitutions in the XML configuration file. If a {@link optimimizer.Config} object is provided then this parameter is ignored.
+             * @return {void}
+             */
             configure: function(config, params) {
                 var pageOptimizer = this.createPageOptimizer(config, params);
                 this.pageOptimizer = pageOptimizer;
             },
             
+            /**
+             * Creates a new instance of a page optimizer using the provided configuration and params.
+             * 
+             * @param config {optimizer.Config|String|files.File|resources.Resource} config The configuration to use
+             * @param params {Object} params An object with name/value pairs that are used for variable substitutions in the XML configuration file. If a {@link optimimizer.Config} object is provided then this parameter is ignored.
+             * @returns {optimizer.PageOptimizer} A new instance of a configured page optimizer
+             */
             createPageOptimizer: function(config, params) {
                 if (!config) {
                     config = defaultConfig;
@@ -48,6 +109,12 @@ raptor.define(
                 return pageOptimizer;
             },
             
+            /**
+             * 
+             * @param configFile {files.File|resources.Resource|String} The configuration file to load. Either a File object, a Resource object, or a file path.
+             * @param params {Object} Variables that can be used inside the XML file in the following format:  s${&ltvariable-name}
+             * @returns {optimizer.Config} The configuration object
+             */
             loadConfigXml: function(configFile, params) {
                 var Config = raptor.require('optimizer.Config');
                 var Resource = raptor.require('resources.Resource');
@@ -72,7 +139,11 @@ raptor.define(
                 return config;
             },
 
-            
+            /**
+             * Helper method to walk all dependencies recursively
+             * 
+             * @param options
+             */
             forEachInclude: function(options) {
     
                
@@ -163,24 +234,62 @@ raptor.define(
                 }
             },
             
+            /**
+             * This method will update a context object to enable the provided extension so that the set of enabled
+             * extensions for the context can later be retrieved using  {@link optimizer#getEnabledExtensionsForContext}
+             * 
+             * This method expects an object that supports a <code>getAttributes()</code> method (typically a {@link templating.Context} object).
+             * 
+             * @param context {Object} The context object to update
+             * @param extension {String} The extension to enable
+             */
             enableExtensionForContext: function(context, extension) {
-                var extensions = context.getAttributes().optimizerExtensions;
+                if (context.getAttributes) {
+                    context = context.getAttributes();
+                }
+                var extensions = context.optimizerExtensions;
                 if (!extensions) {
                     extensions = packager.createExtensionCollection();
                 }
                 extensions.add(extension);
             },
             
+            /**
+             * This method will update a context object to disable the provided extension so that the set of enabled
+             * extensions for the context can later be retrieved using  {@link optimizer#getEnabledExtensionsForContext}
+             * 
+             * This method expects an object that supports a <code>getAttributes()</code> method (typically a {@link templating.Context} object).
+             * 
+             * @param context {Object} The context object to update
+             * @param extension {String} The extension to disable
+             */
             disableExtensionForContext: function(context, extension) {
-                var extensions = context.getAttributes().optimizerExtensions;
+                if (context.getAttributes) {
+                    context = context.getAttributes();
+                }
+                
+                var extensions = context.optimizerExtensions;
                 if (extensions) {
                     extensions.remove(extension);    
                 }
                 
             },
             
+            /**
+             * Returns the set of enabled extensions associated with the provided context.
+             * 
+             * @param context {Object}
+             * @returns {packager.ExtensionCollection} The set of enabled extensions.
+             * 
+             * @see {@link optimizer#enableExtensionForContext}
+             * @see {@link optimizer#disableExtensionForContext}
+             */
             getEnabledExtensionsForContext: function(context) {
-                return context.getAttributes().optimizerExtensions;
+                if (context.getAttributes) {
+                    context = context.getAttributes();
+                }
+                
+                return context.optimizerExtensions;
             }
         }; //end optimizer
 
