23f115a29c625ac25af76c670f01e42d9cb2ba96
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-12-13 19:53:17 -0800

    Updating syntax to raptor-optimizer.js based on migration to AMD

--- bin/raptor-optimizer.js
+++ bin/raptor-optimizer.js
@@ -2,16 +2,13 @@ if( !process.env.NODE_ENV ) process.env.NODE_ENV = 'test';
 var path = require('path');
 var fs   = require('fs');
 
+require('raptor');
 
-require("raptor").createRaptor({
-    logging: {
-        loggers: {
-            'ROOT': {level: 'WARN'},
-            'oop-server': {level: 'WARN'},
-            "optimizer": {level: "INFO"},
-            "optimizer.cli": {level: "INFO"},
-            'resources': {level: 'WARN'}
-        }
+require('raptor/logging').configure({
+    loggers: {
+        'ROOT': {level: 'WARN'},
+        "raptor/optimizer": {level: "INFO"},
+        "raptor/optimizer/cli": {level: "INFO"},
     }
 });
 
