46110e432de6934f0102daa4c8c36a8c0f33c5fc
Patrick Steele-Idem <psteeleidem@ebay.com>
2013-01-16 22:20:55 -0800

    Updated dev spec

--- test/raptor-dev-spec.js
+++ test/raptor-dev-spec.js
@@ -3,10 +3,15 @@ require('./_helper.js');
 var raptor = require('raptor');
 var define = raptor.createDefine(module);
 
+var logger = require('raptor/logging').logger('raptor-dev-spec'),
+    compileAndLoad = helpers.templating.compileAndLoad,
+    compileAndRender = helpers.templating.compileAndRender;
+
 describe('dev spec', function() {
 
 
-    xit("should allow for X", function() {
-        
+    xit("should allow for using templates to render custom tags", function() {
+        var output = compileAndRender("/test-templates/template-as-tag.rhtml", {title: "My Page Title"});
+        expect(output).toEqual('<div><h1>My Page Title</h1></div>');        
     });
 });
