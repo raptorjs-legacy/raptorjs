3132810dbc7c0b24d5bf4f260dd19218332b38ad
Patrick Steele-Idem <psteeleidem@ebay.com>
2013-01-16 21:43:44 -0800

    Bug fixes for "getWidget()" in RenderResult and added test case

--- test/raptor-component-renderer-browser-spec.js
+++ test/raptor-component-renderer-browser-spec.js
@@ -137,4 +137,50 @@ describe('raptor/component-renderer module in the browser', function() {
         });
 
     });
+
+    it('should allow rendering a UI component and getting back the initialized widget', function() {
+
+        jsdomWrapper({
+            html: compileAndRender('/pages/component-renderer/ComponentRendererTest1Page.rhtml'),
+            require: [
+               '/js/jquery-1.8.3.js',
+               'raptor',
+               'raptor/widgets',
+               'raptor/component-renderer',
+               'taglibs/widgets',
+               'pages/component-renderer/ComponentRendererTest1Page',
+            ],
+            ready: function(window, done) {
+                var require = window.require,
+                    document = window.document;
+
+                window.initWidgets();
+                window.$(function() {
+                    try
+                    {
+                        var targetEl = document.getElementById('renderTarget');
+
+                        expect(targetEl).toNotEqual(null);
+                        
+                        var componentRenderer = require('raptor/component-renderer');
+
+                        var Component1Widget = window.require('components/component-renderer/Component1/Component1Widget');
+                        var component1Widget = componentRenderer.render('components/component-renderer/Component1', { id: 'appendTo'}).appendTo(targetEl).getWidget();
+                        expect(component1Widget).toNotEqual(undefined);
+                        expect(component1Widget instanceof Component1Widget).toEqual(true);
+                        done();
+                        
+                    }
+                    catch(e) {
+                        done(e);
+                    }
+                    
+                    
+                });
+                
+                
+            }
+        });
+
+    });
 });
