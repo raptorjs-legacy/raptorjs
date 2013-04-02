fba2cb909068eca001ba8f77be8d5192605f0879
Phil Gates-Idem <phillip.idem@gmail.com>
2013-04-02 12:48:12 -0400

    Fixing assumption that test is ran from within psteeleidem file path

--- test/raptor-files-spec.js
+++ test/raptor-files-spec.js
@@ -17,9 +17,11 @@ describe('files spec', function() {
 
     it("should allow for converting file paths to URLs", function() {
         var url = files.fileUrl(__filename);
-        expect(url).toEqual('file:///Users/psteeleidem/development/ebay/github/Raptor/RaptorJSOpenSource/test/raptor-files-spec.js');
+        expect(url.startsWith('file:///')).toEqual(true);
+        expect(url.endsWith('/test/raptor-files-spec.js')).toEqual(true);
 
         url = files.fileUrl(__dirname + '/with spaces.js');
-        expect(url).toEqual('file:///Users/psteeleidem/development/ebay/github/Raptor/RaptorJSOpenSource/test/with%20spaces.js');
+        expect(url.startsWith('file:///')).toEqual(true);
+        expect(url.endsWith('/test/with%20spaces.js')).toEqual(true);
     });
 });
