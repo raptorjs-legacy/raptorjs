e29081e0460133dfc81c99875c9843d54c67fdd3
Phil Gates-Idem <phillip.idem@gmail.com>
2013-06-27 15:28:40 -0400

    Rewriting logging module to support multiple appenders

--- test/raptor-logging-spec.js
+++ test/raptor-logging-spec.js
@@ -16,43 +16,28 @@ describe('logging module', function() {
         });
     });
     
-    it('should allow any object to become a logger', function() {
-        
-        var obj = {};
-        require('raptor/logging').makeLogger(obj, 'TestLoggingObject');
-        expect(obj.error).toNotEqual(undefined);
-        var error = null;
-        try {
-            obj.error('Test debug');
-        }
-        catch(e) {
-            error = e;
-        }
-        expect(error).toEqual(null);        
-     });
-    
     it('should handle logger prefixes correctly', function() {
         
-        var logLevels = require('raptor/logging').levels;
+        var LogLevel = require('raptor/logging').LogLevel;
         
         var loggerConfigs = require('raptor/logging').getLoggerConfigs();
 
         expect(loggerConfigs.length).toEqual(4);
-        expect(loggerConfigs[0].prefix).toEqual('TestLoggingObject/test22');
-        expect(loggerConfigs[0].level).toEqual(logLevels.INFO);
-        expect(loggerConfigs[1].prefix).toEqual('TestLoggingObject/test1');
-        expect(loggerConfigs[1].level).toEqual(logLevels.DEBUG);
-        expect(loggerConfigs[2].prefix).toEqual('TestLoggingObject');
-        expect(loggerConfigs[2].level).toEqual(logLevels.WARN);
-        expect(loggerConfigs[3].prefix).toEqual('ROOT');
-        expect(loggerConfigs[3].level).toEqual(logLevels.ERROR);
+        expect(loggerConfigs[0].loggerName).toEqual('TestLoggingObject/test22');
+        expect(loggerConfigs[0].logLevel).toEqual(LogLevel.INFO);
+        expect(loggerConfigs[1].loggerName).toEqual('TestLoggingObject/test1');
+        expect(loggerConfigs[1].logLevel).toEqual(LogLevel.DEBUG);
+        expect(loggerConfigs[2].loggerName).toEqual('TestLoggingObject');
+        expect(loggerConfigs[2].logLevel).toEqual(LogLevel.WARN);
+        expect(loggerConfigs[3].loggerName).toEqual('ROOT');
+        expect(loggerConfigs[3].logLevel).toEqual(LogLevel.ERROR);
         
         //console.log("Logging configs:");
         //console.log(loggerConfigs);
         
-        expect(require('raptor/logging').getLogLevel('Test')).toEqual(logLevels.ERROR);
-        expect(require('raptor/logging').getLogLevel('TestLoggingObject')).toEqual(logLevels.WARN);
-        expect(require('raptor/logging').getLogLevel('TestLoggingObject/test1')).toEqual(logLevels.DEBUG);
-        expect(require('raptor/logging').getLogLevel('TestLoggingObject/test22')).toEqual(logLevels.INFO);
+        expect(require('raptor/logging').getLoggerConfig('Test').logLevel).toEqual(LogLevel.ERROR);
+        expect(require('raptor/logging').getLoggerConfig('TestLoggingObject').logLevel).toEqual(LogLevel.WARN);
+        expect(require('raptor/logging').getLoggerConfig('TestLoggingObject/test1').logLevel).toEqual(LogLevel.DEBUG);
+        expect(require('raptor/logging').getLoggerConfig('TestLoggingObject/test22').logLevel).toEqual(LogLevel.INFO);
      });
 });
